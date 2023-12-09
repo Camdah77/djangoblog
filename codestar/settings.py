f8756e5ad1694e7d01c2439e4f6f5d7abc9b012c
Camilla Dahlström <camilla.dahlstromz@gmail.com>
2023-12-09 08:44:39 +0000

    CSRF_TRUSTED_ORIGINS added in settings. Now assing post in admin panel works

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,13 +28,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["8000-camdah77-djangoblog-dobh9pkq2kj.ws-eu106.gitpod.io", 
                 'https://camillablog-6bb3d7ef0a6c.herokuapp.com/',
                 "camillablog.herokuapp.com", "localhost",]
 
-
+CSRF_TRUSTED_ORIGINS = ["https://8000-camdah77-djangoblog-dobh9pkq2kj.ws-eu106.gitpod.io", 
+                         "https://camillablog.herokuapp.com"]
 # Application definition
 
 INSTALLED_APPS = [
